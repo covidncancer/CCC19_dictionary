b11b342e216debcafadc0442bd174012674c6df5
Jeremy Warner <jeremy.warner@vumc.org>
2022-03-04 15:44:51 -0600

    Update Derived variables.R
    Threshold for high missingness raised given introduction of two new variables

--- Derived variables.R
+++ Derived variables.R
@@ -12976,7 +12976,7 @@ var.log <- data.frame(name = character(),
       #Create density plot of missingness
       x <- density(ccc19x$missing[ccc19x$redcap_repeat_instrument == ''])
       plot(x)
-      threshold <- 124
+      threshold <- 126
       abline(v = threshold, col = 'red', lty = 2)
       
       ccc19x$meta_quality[which(ccc19x$missing > threshold)] <- ccc19x$meta_quality[which(ccc19x$missing > threshold)] + 5
